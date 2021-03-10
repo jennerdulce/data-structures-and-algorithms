bba47bf059a93cb5614e7fcac1cf9ceebf0c5bbb
Jenner Dulce <jennerdulce@gmail.com>
2021-03-09 22:37:33 -0800

    finished code challenge along with Stretch Goal

--- javascript/code-challenges/Data-Structures/linkedList/__tests__/lib/ll.test.js
+++ javascript/code-challenges/Data-Structures/linkedList/__tests__/lib/ll.test.js
@@ -3,7 +3,7 @@
 const LinkedList = require('../../lib/ll.js');
 
 
-describe('LinkedList TESTS', () => {
+describe('LinkedList BASIC TESTS', () => {
 
   it('should successfully instantiate an empty linked list', () => {
     let newLL = new LinkedList();
@@ -13,14 +13,14 @@ describe('LinkedList TESTS', () => {
   it('should properly insert into the linked list', () => {
     let newLL = new LinkedList();
     let val = 100;
-    newLL.insert(val);
+    newLL.insertFront(val);
     expect(newLL.head.value).toEqual(val);
   });
 
   it('The head property will properly point to the first node in the linked list', () => {
     let newLL = new LinkedList();
     let val = 100;
-    let newNode = newLL.insert(val);
+    let newNode = newLL.insertFront(val);
     expect(newLL.head.value).toEqual(100);
   });
 
@@ -28,8 +28,8 @@ describe('LinkedList TESTS', () => {
     let newLL = new LinkedList();
     let valOne = 100;
     let valTwo = 200;
-    newLL.insert(valOne);
-    newLL.insert(valTwo);
+    newLL.insertFront(valOne);
+    newLL.insertFront(valTwo);
     expect(newLL.head.value).toEqual(valTwo);
     expect(newLL.head.next.value).toEqual(valOne);
   });
@@ -39,9 +39,9 @@ describe('LinkedList TESTS', () => {
     let valOne = 100;
     let valTwo = 200;
     let valThree = 300;
-    newLL.insert(valOne);
-    newLL.insert(valTwo);
-    newLL.insert(valThree);
+    newLL.insertFront(valOne);
+    newLL.insertFront(valTwo);
+    newLL.insertFront(valThree);
     expect(newLL.includes(valThree)).toEqual(true);
   });
 
@@ -50,9 +50,9 @@ describe('LinkedList TESTS', () => {
     let valOne = 100;
     let valTwo = 200;
     let valThree = 300;
-    newLL.insert(valOne);
-    newLL.insert(valTwo);
-    newLL.insert(valThree);
+    newLL.insertFront(valOne);
+    newLL.insertFront(valTwo);
+    newLL.insertFront(valThree);
     expect(newLL.includes(500)).toEqual(false);
   });
 
@@ -61,9 +61,9 @@ describe('LinkedList TESTS', () => {
     let valOne = 100;
     let valTwo = 200;
     let valThree = 300;
-    newLL.insert(valOne);
-    newLL.insert(valTwo);
-    newLL.insert(valThree);
+    newLL.insertFront(valOne);
+    newLL.insertFront(valTwo);
+    newLL.insertFront(valThree);
     expect(newLL.toString()).toEqual('{ 300 } -> { 200 } -> { 100 } -> NULL');
   });
 
@@ -72,3 +72,115 @@ describe('LinkedList TESTS', () => {
     expect(newLL.includes(1)).toBeFalsy();
   });
 });
+
+
+describe('LinkedList BASIC TESTS', () => {
+
+  it('should successfully instantiate an empty linked list', () => {
+    let newLL = new LinkedList();
+    expect(newLL.head).toBeNull();
+  });
+
+  it('should properly insert into the linked list', () => {
+    let newLL = new LinkedList();
+    let val = 100;
+    newLL.insertFront(val);
+    expect(newLL.head.value).toEqual(val);
+  });
+
+  it('The head property will properly point to the first node in the linked list', () => {
+    let newLL = new LinkedList();
+    let val = 100;
+    let newNode = newLL.insertFront(val);
+    expect(newLL.head.value).toEqual(100);
+  });
+
+  it('Can properly insert multiple nodes into the linked list', () => {
+    let newLL = new LinkedList();
+    let valOne = 100;
+    let valTwo = 200;
+    newLL.insertFront(valOne);
+    newLL.insertFront(valTwo);
+    expect(newLL.head.value).toEqual(valTwo);
+    expect(newLL.head.next.value).toEqual(valOne);
+  });
+
+  it('should return true when finding a value within the linked list that exists', () => {
+    let newLL = new LinkedList();
+    let valOne = 100;
+    let valTwo = 200;
+    let valThree = 300;
+    newLL.insertFront(valOne);
+    newLL.insertFront(valTwo);
+    newLL.insertFront(valThree);
+    expect(newLL.includes(valThree)).toEqual(true);
+  });
+
+  it('should return false when searching for a value in the linked list that does not exist', () => {
+    let newLL = new LinkedList();
+    let valOne = 100;
+    let valTwo = 200;
+    let valThree = 300;
+    newLL.insertFront(valOne);
+    newLL.insertFront(valTwo);
+    newLL.insertFront(valThree);
+    expect(newLL.includes(500)).toEqual(false);
+  });
+
+  it('Can properly return a collection of all the values that exist in the linked list', () => {
+    let newLL = new LinkedList();
+    let valOne = 100;
+    let valTwo = 200;
+    let valThree = 300;
+    newLL.insertFront(valOne);
+    newLL.insertFront(valTwo);
+    newLL.insertFront(valThree);
+    expect(newLL.toString()).toEqual('{ 300 } -> { 200 } -> { 100 } -> NULL');
+  });
+
+  it('should return false if LinkedList is empty', () => {
+    let newLL = new LinkedList();
+    expect(newLL.includes(1)).toBeFalsy();
+  });
+});
+
+
+//====================================================================================
+
+describe('LinkedList INSERTIONS TESTS', () => {
+
+  it('Can successfully add a node to the end of the linked list', () => {
+    let newLL = new LinkedList();
+    newLL.append(100).append(200);
+    expect(newLL.head.next.value).toBe(200);
+  });
+
+  it('Can successfully add multiple nodes to the end of a linked list', () => {
+    let newLL = new LinkedList();
+    newLL.append(100).append(200).append(300);
+    expect(newLL.head.next.next.value).toBe(300);
+  });
+
+  it('Can successfully insert a node before a node located in the middle of a linked list', () => {
+    let newLL = new LinkedList();
+    newLL.append(100).append(200).append(300);
+    newLL.insertBefore(200, 999);
+    expect(newLL.head.next.value).toBe(999);
+  });
+
+  it('Can successfully insert a node before the first node of a linked list', () => {
+    let newLL = new LinkedList();
+    newLL.append(100).append(200).append(300);
+    newLL.insertBefore(100, 999);
+    expect(newLL.head.value).toBe(999);
+  });
+
+  it('Can successfully insert a node after the last node of the linked list', () => {
+    let newLL = new LinkedList();
+    newLL.append(100).append(200).append(300);
+    newLL.insertAfter(300, 999);
+    expect(newLL.toString()).toBe('{ 100 } -> { 200 } -> { 300 } -> { 999 } -> NULL');
+  });
+});
+
+
--- javascript/code-challenges/Data-Structures/linkedList/lib/ll.js
+++ javascript/code-challenges/Data-Structures/linkedList/lib/ll.js
@@ -7,54 +7,122 @@ class LinkedList {
     this.head = null;
   }
 
-  insert(value) {
-    // create a new node
-    let node = new Node(value);
+  delete(value) {
+    let current = this.head;
+    if (this.head.value === value) {
+      let newHead = this.head.next;
+      this.head = newHead;
+      return this;
+    }
+    while (current.next) {
+      if (current.next.value === value) {
+        let updateNode = current.next.next;
+        current.next = updateNode;
+        return this;
+      }
+      current = current.next;
+    }
+    return `${value} is not in the Linked List`;
+  }
 
-    // check to see if empty
+  insertFront(value) {
+    let node = new Node(value);
+    // if empty
     if (!this.head) {
       this.head = node;
     } else {
-      // keeping the value of the old head
       let oldHead = this.head;
-
-      // replace head with the new node
       this.head = node;
-
-      // value of new head = oldHead
       this.head.next = oldHead;
     }
     return this;
   }
 
+  append(value) {
+    let node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      // start at the beginning traverse to the end
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    return this;
+  }
+
+  insertBefore(value, newValue) {
+    let current = this.head;
+    let node = new Node(newValue);
+    if (current.value === value) {
+      let oldHead = this.head;
+      this.head = node;
+      this.head.next = oldHead;
+      return this;
+    } else {
+      while (current.next) {
+        if (current.next.value === value) {
+          node.next = current.next;
+          current.next = node;
+          return this;
+        }
+        current = current.next;
+      }
+    }
+    return `${value} is not in the Linked List`;
+  }
+
+
+  insertAfter(value, newValue) {
+    let current = this.head;
+    let node = new Node(newValue);
+    if (!this.head) {
+      this.head = node;
+      return this;
+    } else {
+      while (current) {
+        if (current.value === value) {
+          // target newly created node's 'NEXT' property
+          node.next = current.next;
+          current.next = node;
+          return this;
+        } else {
+          current = current.next;
+        }
+      }
+    }
+    return `${value} is not in the Linked List`;
+  }
+
   includes(value) {
     if (!this.head) {
       return false;
-    }
-
-    let current = this.head;
-    while (current) {
-      if (current.value === value) {
-        return true;
+    } else {
+      // start at the head
+      let current = this.head;
+      while (current) {
+        if (current.value === value) {
+          return true;
+        }
+        current = current.next;
       }
-      current = current.next;
     }
     return false;
-
   }
 
   toString() {
-    let string = '';
     let current = this.head;
+    let string = '';
     while (current) {
       string += `{ ${current.value} } -> `;
-
       current = current.next;
     }
-
     string += 'NULL';
     return string;
   }
 }
 
 module.exports = LinkedList;
+
