8bcb8822445e9f215f2e5c90d4efac99328b3550
Jenner Dulce <jennerdulce@gmail.com>
2021-06-05 15:31:16 -0700

    something

--- javascript/code-challenges/quickSort/quickSort.js
+++ javascript/code-challenges/quickSort/quickSort.js
@@ -4,7 +4,7 @@ function quickSort(arr, left, right) {
   let position;
   if (arr.length > 1) {
     position = partition(arr, left, right);
-
+    console.log('POSITION =========== ', position);
     if (left < position - 1) {
       quickSort(arr, left, position - 1);
     }
@@ -14,11 +14,14 @@ function quickSort(arr, left, right) {
       quickSort(arr, position, right);
     }
   }
+
   return arr;
 }
 
 function partition(arr, left, right) {
   let pivot = Math.floor((right + left) / 2);
+  console.log('ARRAY =========== ', arr);
+  console.log('PIVOT =========== ', pivot);
   let i = left;
   let j = right;
   while (i <= j) {
@@ -29,12 +32,14 @@ function partition(arr, left, right) {
       j--;
     }
     if (i <= j) {
+      console.log(`SWAPPING LEFT: ${arr[i]} and RIGHT: ${arr[j]}`);
       swap(arr, i, j);
       i++;
       j--;
+      console.log( arr);
     }
   }
-
+  console.log('I =========== ', i);
   return i;
 }
 
@@ -44,6 +49,9 @@ function swap(arr, left, right) {
   arr[left] = arr[right];
   arr[right] = temp;
 }
+let arr = [4,5,3,1,2];
+quickSort(arr, 0, arr.length-1);
+
 
 module.exports = quickSort;
 
