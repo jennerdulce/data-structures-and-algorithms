ce50d516afd0336a12282688da951b9953835aea
Jenner Dulce <jennerdulce@gmail.com>
2021-06-05 15:37:37 -0700

    added tree-intersection

---
+++ javascript/code-challenges/tree-intersection/tree-intersection.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const BST = require("./binary-search-tree.js");
+
+function treeIntersection(bt1, bt2) {
+  let values = {};
+  let arr = [];
+  let nodes1 = bt1.preOrder();
+  let nodes2 = bt2.preOrder();
+
+  for (let i = 0; i < nodes1.length; i++) {
+    console.log(nodes1[i])
+    values[nodes1[i].toString()] = true;
+  }
+
+  for (let i = 0; i < nodes2.length; i++) {
+    if (values[nodes2[i].toString()]) {
+      arr.push(nodes2[i]);
+    }
+  }
+  console.log(values)
+
+  return arr;
+}
+
+const bt1 = new BST();
+const bt2 = new BST();
+
+bt1.add(12).add(5).add(7).add(9).add(100).add(1);
+bt2.add(5).add(8).add(9).add(7).add(6).add(1);
+
+console.log(treeIntersection(bt1, bt2));
---
+++ javascript/code-challenges/tree-intersection/tree.js
@@ -0,0 +1,126 @@
+"use strict";
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  preOrder() {
+    let nodes = [];
+
+    let _walk = (node) => {
+      nodes.push(node.value); // add node once leaf is found on left
+
+      if (node.left) {
+        _walk(node.left);
+      }
+
+      if (node.right) {
+        _walk(node.right);
+      }
+    };
+
+    _walk(this.root);
+
+    return nodes;
+  }
+
+  inOrderWithoutHelper(node, nodes = []) {
+    // Traverse
+    if (node.left) {
+      this.inOrderWithoutHelper(node.left, nodes);
+    }
+
+    // Do the Work!
+    nodes.push(node.value);
+
+    // Traverse
+    if (node.right) {
+      this.inOrderWithoutHelper(node.right, nodes);
+    }
+
+    return nodes;
+  }
+
+  inOrder() {
+    let nodes = [];
+
+    let _walk = (node) => {
+      if (node.left) {
+        _walk(node.left);
+      }
+
+      nodes.push(node.value);
+
+      if (node.right) {
+        _walk(node.right);
+      }
+    };
+
+    _walk(this.root);
+
+    return nodes;
+  }
+
+  postOrder() {
+    let nodes = [];
+
+    let _walk = (node) => {
+      if (node.left) {
+        _walk(node.left);
+      }
+
+      if (node.right) {
+        _walk(node.right);
+      }
+
+      nodes.push(node.value);
+    };
+
+    _walk(this.root);
+
+    return nodes;
+  }
+
+  breadthFirst() {
+    let nodes = [];
+    let queue = [];
+
+    queue.push(this.root); // queue.enque();
+
+    while (queue.length) {
+      // while queue.peek()
+      // Do the work ...
+      let currentNode = queue.shift(); // queue.dequeue();
+      nodes.push(currentNode.value);
+
+      // Keep traversing by queueing up the left and right
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+    }
+
+    return nodes;
+  }
+  findMaxValue() {
+    let max = this.root.value;
+
+    let _walk = (node) => {
+      max = node.value > max ? node.value : max;
+      if (node.left) {
+        _walk(node.left);
+      }
+      if (node.right) {
+        _walk(node.right);
+      }
+    };
+    _walk(this.root);
+
+    return max;
+  }
+}
+
+module.exports = BinaryTree;
